b0d71704d6b707a4ec6d9a0ceb49136fc377fd7d
anhngoc <ngoc2162003@gmail.com>
2022-06-19 14:45:34 +0700

    change request margin

--- src/pages/Home/CompanyHome/CompanyRequest.js
+++ src/pages/Home/CompanyHome/CompanyRequest.js
@@ -5,7 +5,7 @@ export default function CompanyRequest() {
   return (
     <div>
       <h1 className="mt-10 mb-10 sm:text-3xl text-2xl font-medium title-font text-center text-red-900">Your Requests </h1>
-      <div className="accordion" id="accordionExample">
+      <div className="accordion mx-10" id="accordionExample">
         <div className="accordion-item bg-white border border-gray-200">
           <h2 className="accordion-header mb-0" id="headingOne">
             <button className="accordion-button relative flex items-center w-full py-4 px-5 text-base text-red-500 text-left bg-white border-0 rounded-none transition focus:outline-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
