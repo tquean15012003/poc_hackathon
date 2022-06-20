c97a5fea8de706e1b0df85ab517689bca7ea3683
anhngoc <ngoc2162003@gmail.com>
2022-06-21 02:36:10 +0700

    add edit skill, only python

--- src/pages/Home/UserHome/UserProfileComponent/UserSkill.js
+++ src/pages/Home/UserHome/UserProfileComponent/UserSkill.js
@@ -41,9 +41,11 @@ export default function UserSkill() {
                             <div className="relative mb-4 px-3">
                                 <label htmlFor="issuer" className="leading-7 text-sm text-gray-600">Issuer</label>
                                 <select name="issuer" id="issuer" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                    <option value="user">CompA</option>
-                                    <option value="company">UniB</option>
-                                    <option value="admin">SC</option>
+                                    <option value="user">Nanyang Technological University</option>
+                                    <option value="company">AWS</option>
+                                    <option value="company">Coursera</option>
+                                    <option value="company">LinkedIn</option>
+                                    <option value="company">Udemy</option>
                                 </select>
                             </div>
                             <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
@@ -67,7 +69,40 @@ export default function UserSkill() {
                             <p className="text-gray-700 text-sm mb-4">
                                 Level 3 (<span className="text-sm text-green-500">Verified</span>)
                             </p>
-                            <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Edit</button>
+                            <p className="text-gray-700 text-sm mb-4">
+                                Issued by: Nanyang Technological University
+                            </p>
+                            <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#editVeModal">Edit</button>
+                            <div className="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="editVeModal" tabIndex={-1} aria-labelledby="exampleModalLongLabel" aria-hidden="true">
+                                <div className="modal-dialog relative w-auto pointer-events-none">
+                                    <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                                        <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+                                            <h5 className="text-xl font-medium leading-normal text-gray-800" id="exampleModalLongLabel">
+                                                Edit skill
+                                            </h5>
+                                            <button type="button" className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="modal" aria-label="Close" />
+                                        </div>
+                                        <div className="relative mb-4 px-3">
+                                            <label htmlFor="issuer" className="leading-7 text-sm text-gray-600">Do you want to add another issuer?</label>
+                                            <select name="issuer" id="issuer" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                <option value="user">Nanyang Technological University</option>
+                                                <option value="company">AWS</option>
+                                                <option value="company">Coursera</option>
+                                                <option value="company">LinkedIn</option>
+                                                <option value="company">Udemy</option>
+                                            </select>
+                                        </div>
+                                        <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+                                            <button type="button" className="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-dismiss="modal">
+                                                Close
+                                            </button>
+                                            <button type="button" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1">
+                                                Edit
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="flex justify-center">
@@ -75,6 +110,9 @@ export default function UserSkill() {
                             <h5 className="text-gray-900 text-lg leading-tight font-medium mb-2">C++</h5>
                             <p className="text-gray-700 text-sm mb-4">
                                 Level 2 (<span className="text-sm text-green-500">Verified</span>)
+                            </p>
+                            <p className="text-gray-700 text-sm mb-4">
+                                Issued by: AWS, LinkedIn
                             </p>
                             <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Edit</button>
                         </div>
@@ -85,6 +123,9 @@ export default function UserSkill() {
                             <p className="text-gray-700 text-sm mb-4">
                                 Level 4 (<span className="text-sm text-yellow-500">Pending review</span>)
                             </p>
+                            <p className="text-gray-700 text-sm mb-4">
+                                Issued by:
+                            </p>
                             <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Edit</button>
                         </div>
                     </div>
@@ -93,6 +134,9 @@ export default function UserSkill() {
                             <h5 className="text-gray-900 text-lg leading-tight font-medium mb-2">Business Administrator</h5>
                             <p className="text-gray-700 text-sm mb-4">
                                 Level 1 (<span className="text-sm text-green-500">Verified</span>)
+                            </p>
+                            <p className="text-gray-700 text-sm mb-4">
+                                Issued by: Coursera
                             </p>
                             <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Edit</button>
                         </div>
