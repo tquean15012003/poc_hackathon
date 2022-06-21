fe6a43367431319cd71e9f63619aa0cf8c7bf9eb
Harry <tquean15012003@gmail.com>
2022-06-21 07:37:36 +0700

    fix sign up

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -68,6 +68,7 @@ export default function Register() {
               <select onChange={formik.handleChange} value={formik.values.type} onBlur={formik.handleBlur} name="type" id="type" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                 <option value="user">User</option>
                 <option value="company">Company</option>
+                <option value="admin">Admin</option>
               </select>
               {formik.touched.type && formik.errors.type ? (<div className="text-red-900"> {formik.errors.type}</div>) : null}
             </div>
